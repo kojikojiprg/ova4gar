84888f5854ae27d1dc457d9dfe9c191f696b1941
kojikojiprg <kojikoji.prg@gmail.com>
2020-07-03 22:37:22 +0900

    write video

--- python/basketball.py
+++ python/basketball.py
@@ -5,10 +5,11 @@ from module import common, video, utils, keypoint, tracker
 
 if __name__ == '__main__':
     video_path = common.data_dir + 'basketball/basketball_alphapose.mp4'
+    out_path = common.out_dir + 'basketball/basketball_particle.mp4'
     court_path = common.data_dir + 'basketball/court.png'
     json_path = common.data_dir + 'basketball/keypoints.json'
 
-    video = video.Video(video_path)
+    video = video.Video(video_path, out_path)
     court = cv2.imread(court_path)
 
     keypoints_frame = keypoint.Frame(json_path)
@@ -19,8 +20,10 @@ if __name__ == '__main__':
     tr = tracker.Tracker(keypoints_frame, ((15, 350), (1080, 632)))
     result = tr.track_person(person_id)
 
+    frames = []
     for i, r in enumerate(result):
         frame = video.read()
         if r is not None:
             cv2.circle(frame, tuple(r), 7, (0, 0, 255), thickness=-1)
-        utils.show_img(frame)
+        frames.append(frame)
+    video.write(frames)
