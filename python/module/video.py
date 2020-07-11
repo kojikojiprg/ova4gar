3b8cd953f116974786c420940b3aed68cd4b0898
kojikojiprg <kojikoji.prg@gmail.com>
2020-07-11 14:13:35 +0900

    modify write

--- python/module/video.py
+++ python/module/video.py
@@ -2,22 +2,13 @@ import cv2
 
 
 class Video:
-    def __init__(self, in_path, out_path):
+    def __init__(self, in_path):
         self._video = cv2.VideoCapture(in_path)
-
-        # image size
-        self.width = int(self._video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.height = int(self._video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.size = (self.width, self.height)
 
         # video info
         self.frame_count = int(self._video.get(cv2.CAP_PROP_FRAME_COUNT))
         self.frame_rate = int(self._video.get(cv2.CAP_PROP_FPS))
         self.frame_num = self._video.get(cv2.CAP_PROP_FRAME_COUNT)
-
-        # writer object
-        fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-        self._writer = cv2.VideoWriter(out_path, fmt, self.frame_rate, self.size)
 
     def __del__(self):
         self._video.release()
@@ -29,7 +20,13 @@ class Video:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # BGR to RGB
         return frame
 
-    def write(self, frames):
+    def write(self, frames, out_path, size):
+
+        # writer object
+        fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+        self._writer = cv2.VideoWriter(
+            out_path, fmt, self.frame_rate, size)
+
         for frame in frames:
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # BGR to RGB
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # RGB to BGR
             self._writer.write(frame)
