c0b9e5f05656df2c6e9b3383ed727f2ee4c6a7c2
kojikojiprg <kojikoji.prg@gmail.com>
2020-08-15 11:04:11 +0900

    del track person

--- python/module/tracker.py
+++ python/module/tracker.py
@@ -11,29 +11,6 @@ class Tracker:
         for i, keypoints in enumerate(keypoints_lst):
             self.persons.append(Person(i, keypoints))
 
-    def track_person(self, person_id):
-        person = self.person[person_id]
-        for keypoints_lst in self.keypoints_frame_lst:
-            targets = keypoints_lst.get_middle_points('Hip')
-
-            point = None
-            keypoints = None
-            nearest = np.inf
-            for i, target in enumerate(targets):
-                # マハラノビス距離を求める
-                distance = person.pf.mahalanobis(target)
-
-                # 一番距離が近いポイントを取り出す
-                if distance < nearest:
-                    point = target
-                    keypoints = keypoints_lst[i]
-                    nearest = distance
-
-            # パーティクルフィルタを更新
-            person.update(point, keypoints)
-
-        return person
-
     def track(self):
         for keypoints_lst in self.keypoints_frame_lst:
             targets = keypoints_lst.get_middle_points('Hip')
