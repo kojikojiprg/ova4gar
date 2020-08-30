ed582b74abf6ec47fbcb746110aa54024dd962c8
kojikojiprg <kojikoji.prg@gmail.com>
2020-08-30 10:33:27 +0900

    modify insert

--- python/common/database.py
+++ python/common/database.py
@@ -1,20 +1,39 @@
-import common as cm
+import numpy as np
 import sqlite3 as sql
 import os
+import io
 
 
 class DataBase:
     def __init__(self, path):
-        self.path = cm.db_dir + path + '.db'
-        os.makedirs(self.path, exist_ok=True)
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, 'w') as f:
+            f.write('')
 
-        self.conn = sql.connect(self.path)
+        # Converts np.array to TEXT when inserting
+        sql.register_adapter(np.ndarray, self._adapt_array)
+
+        # Converts TEXT to np.array when selecting
+        sql.register_converter('array', self._convert_array)
+
+        self.conn = sql.connect(path, detect_types=sql.PARSE_DECLTYPES)
 
     def __del__(self):
         self.conn.commit()
         self.conn.close()
 
-    def _create_table(self, name, cols):
+    def _adapt_array(self, arr):
+        out = io.BytesIO()
+        np.save(out, arr)
+        out.seek(0)
+        return sql.Binary(out.read())
+
+    def _convert_array(self, text):
+        out = io.BytesIO(text)
+        out.seek(0)
+        return np.load(out)
+
+    def create_table(self, name, cols):
         c = self.conn.cursor()
 
         col_syntax = ''
@@ -22,20 +41,53 @@ class DataBase:
             col_syntax += '{} {}, '.format(col, typ)
         col_syntax = col_syntax[:-2]
 
-        syntax = 'create table {}({})'.format(name, col_syntax)
+        syntax = 'create table if not exists {}({})'.format(name, col_syntax)
         c.execute(syntax)
 
         self.conn.commit()
 
-    def _insert_datas(self, name, datas):
+    def drop_table(self, name):
         c = self.conn.cursor()
 
-        datas_syntax = '('
-        for _ in range(len(datas)):
-            datas_syntax += '?,'
-        datas_syntax[-1] = ')'
+        syntax = 'drop table if exists {}'.format(name)
+        c.execute(syntax)
 
-        syntax = 'insert into {} values {}'.format(name, datas_syntax)
+        self.conn.commit()
+
+    def insert_data(self, name, cols, data):
+        c = self.conn.cursor()
+
+        col_syntax = ''
+        data_syntax = ''
+        for col in cols:
+            col_syntax += '{}, '.format(col)
+            data_syntax += '?,'
+        col_syntax = col_syntax[:-2]
+        name_syntax = '{} ({})'.format(name, col_syntax)
+
+        data_syntax = data_syntax[:-1]
+        data_syntax = '({})'.format(data_syntax)
+
+        syntax = 'insert into {} values {}'.format(name_syntax, data_syntax)
+        c.execute(syntax, data)
+
+        self.conn.commit()
+
+    def insert_datas(self, name, cols, datas):
+        c = self.conn.cursor()
+
+        col_syntax = ''
+        data_syntax = ''
+        for col in cols:
+            col_syntax += '{}, '.format(col)
+            data_syntax += '?,'
+        col_syntax = col_syntax[:-2]
+        name_syntax = '{} ({})'.format(name, col_syntax)
+
+        data_syntax = data_syntax[:-1]
+        data_syntax = '({})'.format(data_syntax)
+
+        syntax = 'insert into {} values {}'.format(name_syntax, data_syntax)
         c.executemany(syntax, datas)
 
         self.conn.commit()
