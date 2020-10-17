33c034671f1bba286dd17e50b90cc5ce76f71f7a
kojikojiprg <kojikoji.prg@gmail.com>
2020-10-17 15:03:18 +0900

    add person vector

--- python/display/person.py
+++ python/display/person.py
@@ -1,4 +1,7 @@
 from common.keypoint import KeypointsList
+from person.functions import FUNC_DICT
+import numpy as np
+import cv2
 
 
 class Person:
@@ -7,11 +10,60 @@ class Person:
         self.start_frame_num = frame_num
 
         self.keypoints_lst = KeypointsList()
-        self.average_lst = []
-        self.vector_lst = []
+        self.indicators_dict = {k: [] for k in FUNC_DICT.keys()}
+        self.setting_lst = [
+            # arrow_length, color, tip_length
+            [8, (255, 0, 0), 1.0],
+            [10, (0, 0, 255), 1.5]
+        ]
 
-    def append(self, tracking_data):
-        i = 2
-        self.keypoints_lst.append(tracking_data[i])
-        self.average_lst.append(tracking_data[i + 1])
-        self.vector_lst.append(tracking_data[i + 2])
+    def append(self, data):
+        self.keypoints_lst.append(data[2])
+        for i, k in enumerate(FUNC_DICT.keys()):
+            self.indicators_dict[k].append(data[3 + i])
+
+    def display_tracking(self, frame_num, frame):
+        idx = frame_num - self.start_frame_num
+        if idx < 0:
+            return frame
+
+        keypoints = self.keypoints_lst[idx]
+        if keypoints is None:
+            return frame
+
+        point = keypoints.get_middle('Hip')
+        cv2.circle(frame, tuple(point), 7, (0, 0, 255), thickness=-1)
+        cv2.putText(frame, str(self.id), tuple(point),
+                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+
+        return frame
+
+    def display_vector(self, frame_num, field, homo):
+        idx = frame_num - self.start_frame_num
+        if idx < 0:
+            return field
+
+        keypoints = self.keypoints_lst[idx]
+        if keypoints is None:
+            return field
+
+        start = keypoints.get_middle('Hip')
+        for i, v in enumerate(self.indicators_dict.values()):
+            data = v[idx]
+            if data is None:
+                continue
+
+            arrow_length = self.setting_lst[i][0]
+            x = np.cos(data)
+            y = np.sin(data)
+            end = start + np.array([x, y]) * arrow_length
+
+            # ホモグラフィ変換後の矢印の長さを揃える
+            ratio = arrow_length / np.linalg.norm(end - start)
+            end = start + ((end - start) * ratio).astype(int)
+
+            color = self.setting_lst[i][1]
+            tip_length = self.setting_lst[i][2]
+            cv2.arrowedLine(field, tuple(start), tuple(end), color, tipLength=tip_length)
+
+        return field
