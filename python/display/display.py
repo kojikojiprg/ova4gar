e50b0168605a299471aed555c33c9851ddd36701
kojikojiprg <kojikoji.prg@gmail.com>
2020-10-17 15:03:00 +0900

    del indicator

--- python/display/display.py
+++ python/display/display.py
@@ -1,28 +1,23 @@
 from common import database
 from display.person import Person
-from display.indicator import Indicator
 from display.video import Video
-from display.functions import FUNC_DICT
 import numpy as np
 import cv2
 
 
-def display(video_path, out_dir, tracking_db_path, indicator_db_path, field, homography):
+def display(video_path, out_dir, person_db_path, field, homography):
     # out video file paths
-    out_paths = [out_dir + '{}.mp4'.format(database.TRACKING_TABLE.name)]
-    for table in database.INDICATOR_TABLES:
-        out_paths.append(out_dir + '{}.mp4'.format(table.name))
+    out_paths = [
+        out_dir + '{}.mp4'.format(database.TRACKING_TABLE.name),
+        out_dir + '{}.mp4'.format(database.PERSON_TABLE.name)
+    ]
 
     # connect database and read datas
-    tracking_db = database.DataBase(tracking_db_path)
-    indicator_db = database.DataBase(indicator_db_path)
-    persons, indicators = read_sql(tracking_db, indicator_db)
+    person_db = database.DataBase(person_db_path)
+    persons = read_sql(person_db)
 
     # load video
     video = Video(video_path)
-
-    # copy field
-    fields = [field.copy() for _ in range(len(database.INDICATOR_TABLES))]
 
     frames_lst = [[] for _ in range(len(out_paths))]
     for i in range(video.frame_num):
@@ -30,78 +25,39 @@ def display(video_path, out_dir, tracking_db_path, indicator_db_path, field, hom
         frame = video.read()
 
         # フレーム番号を表示
-        cv2.putText(frame, 'Frame:{}'.format(i + 1), (10, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255))
+        cv2.putText(frame, 'Frame:{}'.format(i + 1), (10, 50),
+                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255))
 
-        # トラッキングを表示
+        field_tmp = field.copy()
         for person in persons:
-            idx = i - person.start_frame_num
-            if 0 <= idx and idx < len(person.keypoints_lst):
-                point = person.keypoints_lst[idx]
-                if point is not None:
-                    point = point.get_middle('Hip')
-                    cv2.circle(frame, tuple(point), 7, (0, 0, 255), thickness=-1)
-                    cv2.putText(frame, str(person.id), tuple(point), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+            # トラッキングを表示
+            frame = person.display_tracking(i, frame)
+            # 向きを表示
+            field_tmp = person.display_vector(i, field_tmp, homography)
 
         # append tracking result
         frames_lst[0].append(frame)
-
-        # 指標を表示
-        for indicator_idx, indicator in enumerate(indicators):
-            frame_raw = frame.copy()
-            if indicator.is_reset_display:
-                field_rslt = indicator.display(i, indicator, field.copy(), homography)
-            else:
-                field_rslt = indicator.display(i, indicator, fields[indicator_idx], homography)
-            frame_rslt = combine_image(frame_raw, field_rslt)
-
-            frames_lst[indicator_idx + 1].append(frame_rslt)
+        frames_lst[1].append(combine_image(frame, field_tmp))
 
     for frames, out_path in zip(frames_lst, out_paths):
         video.write(frames, out_path, frames[0].shape[1::-1])
 
 
-def read_sql(tracking_db, indicator_db):
+def read_sql(person_db):
     persons = []
 
-    # tracking data
-    tracking_datas = tracking_db.select(database.TRACKING_TABLE.name)
-    for tracking_data in tracking_datas:
-        person_id = tracking_data[0]
-        frame_num = tracking_data[1]
+    # person data
+    person_datas = person_db.select(database.PERSON_TABLE.name)
+    for data in person_datas:
+        person_id = data[0]
+        frame_num = data[1]
 
         if len(persons) == person_id:
             persons.append(Person(person_id, frame_num))
 
-        persons[person_id].append(tracking_data)
+        persons[person_id].append(data)
 
-    # indicator data
-    indicators = []
-
-    # read all data
-    indicator_dict = {}
-    for table in database.INDICATOR_TABLES:
-        datas = indicator_db.select(table.name)
-        indicator_dict[table.name] = datas
-
-    # append datas
-    for i, items in enumerate(indicator_dict.items()):
-        table_name = items[0]
-        indicator_datas = items[1]
-        indicators.append(
-            Indicator(table_name, FUNC_DICT[table_name][0], FUNC_DICT[table_name][1]))
-
-        for indicator_data in indicator_datas:
-            for idx, key in enumerate(database.INDICATOR_TABLES[i].cols.keys()):
-                if key == 'Frame_No':
-                    break
-            frame_num = indicator_data[idx]
-            indicators[-1].append(frame_num, indicator_data)
-
-    # make heatmaps
-    for indicator in indicators:
-        indicator.make_heatmap()
-
-    return persons, indicators
+    return persons
 
 
 def combine_image(frame, field):
