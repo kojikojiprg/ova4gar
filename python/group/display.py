15b15a016d4da21eca2239ae7cda0bfa98ec8079
kojikojiprg <kojikoji.prg@gmail.com>
2020-11-05 15:41:05 +0900

    add comment

--- python/group/display.py
+++ python/group/display.py
@@ -16,7 +16,10 @@ def display_attention(datas, field, heatmap, min_r=8):
     for data in datas:
         if data[1] is not None:
             for point in data[1]:
+                # 視線が重なったところを黒色で表示
                 cv2.circle(field, tuple(point.astype(int)), 5, (0, 0, 0), thickness=-1)
+
+            # クラスターを表示
             point = np.average(data[1], axis=0).astype(int)
             r = min_r + len(data[1])
             color = heatmap.colormap(len(data[1]))
