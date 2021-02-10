28b31f4b40ef1a49e41ece21ea76d6814ea3ba69
kojikojiprg <kojikoji.prg@gmail.com>
2021-02-10 14:26:31 +0900

    modify

--- python/img_test.py
+++ python/img_test.py
@@ -6,17 +6,16 @@ import cv2
 
 if __name__ == '__main__':
     # file path
-    name = 'basketball'
-    video_path = common.data_dir + '{0}/{0}_alphapose.mp4'.format(name)
-    court_path = common.data_dir + '{}/court.png'.format(name)
+    name = 'or'
+    video_path = common.data_dir + '{0}/sugukesu.png'.format(name)
+    court_path = common.data_dir + '{}/field.png'.format(name)
 
     # open video and image
-    video = video.Video(video_path)
-    court = cv2.imread(court_path)
-
-    utils.show_img(court)
-    frame = video.read()
+    frame = cv2.imread(video_path)
     utils.show_img(frame)
+
+    court = cv2.imread(court_path)
+    utils.show_img(court)
 
     p_video = common.homo[name][0]
     p_court = common.homo[name][1]
