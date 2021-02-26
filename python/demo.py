716af377a606ab12406f3309e13c4a7dfa2ead90
kojikojiprg <kojikoji.prg@gmail.com>
2021-02-26 17:10:50 +0900

    modify

--- python/demo.py
+++ python/demo.py
@@ -6,27 +6,27 @@ from display.display import display
 import cv2
 
 
-IS_TRACKING = False
-IS_PERSON = False
+IS_TRACKING = True
+IS_PERSON = True
 IS_GROUP = True
 IS_DISPLAY = True
 
 
 if __name__ == '__main__':
-    name = 'basketball'
+    name = 'demo'
     video_path = common.data_dir + '{0}/{0}_alphapose.mp4'.format(name)
     out_dir = common.out_dir + '{}/'.format(name)
-    court_path = common.data_dir + '{}/court.png'.format(name)
+    field_path = common.data_dir + '{}/field.png'.format(name)
     keypoints_path = common.data_dir + '{}/keypoints.json'.format(name)
     tracking_db_path = common.db_dir + '{}/tracking.db'.format(name)
     person_db_path = common.db_dir + '{}/person.db'.format(name)
     group_db_path = common.db_dir + '{}/group.db'.format(name)
 
     # homography
-    court_raw = cv2.imread(court_path)
+    field_raw = cv2.imread(field_path)
     p_video = common.homo[name][0]
-    p_court = common.homo[name][1]
-    homo = transform.Homography(p_video, p_court, court_raw.shape)
+    p_field = common.homo[name][1]
+    homo = transform.Homography(p_video, p_field, field_raw.shape)
 
     if IS_TRACKING:
         track(keypoints_path, tracking_db_path, name)
@@ -38,4 +38,4 @@ if __name__ == '__main__':
         gd.make_database(person_db_path, group_db_path, homo)
 
     if IS_DISPLAY:
-        display(video_path, out_dir, person_db_path, group_db_path, court_raw, homo)
+        display(video_path, out_dir, person_db_path, group_db_path, field_raw, homo)
