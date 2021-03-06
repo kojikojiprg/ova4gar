070931195dcd7c829b7253e206b6bb178a0fd3d7
kojikojiprg <kojikoji.prg@gmail.com>
2021-03-06 18:28:21 +0900

    modify

--- module/run.py
+++ module/run.py
@@ -11,25 +11,27 @@ IS_PERSON = True
 IS_GROUP = True
 IS_DISPLAY = True
 
+ROOM_NUM = '09'
+DATE = '20210304'
+NAME = 'gaze1-1'
 
 if __name__ == '__main__':
-    name = 'record'
-    video_path = common.data_dir + '{0}/{0}_alphapose.mp4'.format(name)
-    out_dir = common.out_dir + '{}/'.format(name)
-    field_path = common.data_dir + '{}/field.png'.format(name)
-    keypoints_path = common.data_dir + '{}/keypoints.json'.format(name)
-    tracking_db_path = common.db_dir + '{}/tracking.db'.format(name)
-    person_db_path = common.db_dir + '{}/person.db'.format(name)
-    group_db_path = common.db_dir + '{}/group.db'.format(name)
+    video_path = common.data_dir + '/{0}/{1}/{2}/AlphaPose_{2}.mp4'.format(ROOM_NUM, DATE, NAME)
+    out_dir = common.out_dir + '/{0}/{1}/{2}'.format(ROOM_NUM, DATE, NAME)
+    field_path = common.data_dir + '/field.png'
+    keypoints_path = common.data_dir + '/{0}/{1}/{2}/alphapose-results.json'.format(ROOM_NUM, DATE, NAME)
+    tracking_db_path = common.db_dir + '/{0}/{1}/{2}/tracking.db'.format(ROOM_NUM, DATE, NAME)
+    person_db_path = common.db_dir + '/{0}/{1}/{2}/person.db'.format(ROOM_NUM, DATE, NAME)
+    group_db_path = common.db_dir + '/{0}/{1}/{2}/group.db'.format(ROOM_NUM, DATE, NAME)
 
     # homography
     field_raw = cv2.imread(field_path)
-    p_video = common.homo[name][0]
-    p_field = common.homo[name][1]
+    p_video = common.homo[ROOM_NUM][0]
+    p_field = common.homo[ROOM_NUM][1]
     homo = transform.Homography(p_video, p_field, field_raw.shape)
 
     if IS_TRACKING:
-        track(keypoints_path, tracking_db_path, name)
+        track(keypoints_path, tracking_db_path, NAME)
 
     if IS_PERSON:
         pd.make_database(tracking_db_path, person_db_path, homo)
