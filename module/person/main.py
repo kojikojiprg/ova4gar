7c605a62463cf1568f215bcde3bbf9b0e65c51ee
kojikojiprg <kojikoji.prg@gmail.com>
2021-03-30 11:04:34 +0900

    modify

--- module/person/main.py
+++ module/person/main.py
@@ -8,19 +8,24 @@ def main(tracking_json_path, person_json_path, homo):
     persons = []
     person_datas = []
     for item in tracking_datas.items():
+        # trackingのデータを取得
         person_id = item[TRACKING_FORMAT[0]]
         frame_num = item[TRACKING_FORMAT[1]]
         keypoints = item[TRACKING_FORMAT[2]]
         vector = item[TRACKING_FORMAT[3]]
         average = item[TRACKING_FORMAT[4]]
 
+        # personクラスを新規作成
         if len(persons) == person_id:
             persons.append(Person(person_id, frame_num, homo))
 
+        # 指標を計算
         persons[person_id].calc_indicator(keypoints, vector, average)
 
+        # jsonフォーマットを作成して追加
         data = persons[person_id].to_json(frame_num)
         if data is not None:
             person_datas.append(data)
 
-    json.dump(person_datas)
+    # jsonに書き込み
+    json.dump(person_datas, person_json_path)
