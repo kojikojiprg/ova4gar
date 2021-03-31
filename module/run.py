98cabee481b9b55503a589740ccb94bf5e5a3246
kojikojiprg <kojikoji.prg@gmail.com>
2021-03-31 16:30:12 +0900

    modify

--- module/run.py
+++ module/run.py
@@ -1,20 +1,20 @@
 from common import common, transform
-from tracking.tracking import track
-from person import data as pd
-from group import data as gd
+from tracker import main as tr
+from person import main as pd
+from group import main as gd
 from display.display import display
 import os
 import cv2
 
 
-IS_TRACKING = False
+IS_TRACKING = True
 IS_PERSON = True
 IS_GROUP = True
 IS_DISPLAY = True
 
 ROOM_NUM = '09'
 DATE = '20210304'
-NAME = 'pass3'
+NAME = 'pass1'
 
 
 if __name__ == '__main__':
@@ -22,9 +22,9 @@ if __name__ == '__main__':
     out_dir = os.path.join(common.out_dir, '{0}/{1}/{2}/'.format(ROOM_NUM, DATE, NAME))
     field_path = os.path.join(common.data_dir, 'field.png')
     keypoints_path = os.path.join(common.data_dir, '{0}/{1}/{2}/alphapose-results.json'.format(ROOM_NUM, DATE, NAME))
-    tracking_db_path = os.path.join(common.db_dir, '{0}/{1}/{2}/tracking.db'.format(ROOM_NUM, DATE, NAME))
-    person_db_path = os.path.join(common.db_dir, '{0}/{1}/{2}/person.db'.format(ROOM_NUM, DATE, NAME))
-    group_db_path = os.path.join(common.db_dir, '{0}/{1}/{2}/group.db'.format(ROOM_NUM, DATE, NAME))
+    tracking_json_path = os.path.join(common.json_dir, '{0}/{1}/{2}/tracking.json'.format(ROOM_NUM, DATE, NAME))
+    person_json_path = os.path.join(common.json_dir, '{0}/{1}/{2}/person.json'.format(ROOM_NUM, DATE, NAME))
+    group_json_path = os.path.join(common.json_dir, '{0}/{1}/{2}/group.json'.format(ROOM_NUM, DATE, NAME))
 
     # homography
     field_raw = cv2.imread(field_path)
@@ -33,13 +33,13 @@ if __name__ == '__main__':
     homo = transform.Homography(p_video, p_field, field_raw.shape)
 
     if IS_TRACKING:
-        track(keypoints_path, tracking_db_path)
+        tr.main(keypoints_path, tracking_json_path)
 
     if IS_PERSON:
-        pd.make_database(tracking_db_path, person_db_path, homo)
+        pd.main(tracking_json_path, person_json_path, homo)
 
     if IS_GROUP:
-        gd.make_database(person_db_path, group_db_path, homo)
+        gd.main(person_json_path, group_json_path, homo)
 
     if IS_DISPLAY:
-        display(video_path, out_dir, person_db_path, group_db_path, field_raw, homo)
+        display(video_path, out_dir, person_json_path, group_json_path, field_raw)
