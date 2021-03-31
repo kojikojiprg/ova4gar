3ee1bbb0ee097aa19772121a9443c28b71b049d7
kojikojiprg <kojikoji.prg@gmail.com>
2021-03-31 12:22:12 +0900

    modify

--- module/group/main.py
+++ module/group/main.py
@@ -10,11 +10,11 @@ def main(person_json_path, group_json_path, homo):
     last_frame_num = person_datas[-1]['image_id'] + 1
     for frame_num in range(last_frame_num):
         # フレームごとにデータを取得する
-        frame_person_data = [
+        frame_person_datas = [
             data for data in person_datas if data['image_id'] == frame_num]
 
         # 指標の計算
-        group.calc_indicator(frame_num, frame_person_data)
+        group.calc_indicator(frame_num, frame_person_datas)
 
     # jsonフォーマットを生成して書き込み
     group_datas = group.to_json()
