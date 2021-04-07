f92f57fa3fc56901424ce8861363e60839260acd
kojikojiprg <kojikoji.prg@gmail.com>
2021-04-07 13:02:03 +0900

    argument parser

--- module/run.py
+++ module/run.py
@@ -3,43 +3,65 @@ from tracker import main as tr
 from person import main as pd
 from group import main as gd
 from display.display import display
+import argparse
 import os
 import cv2
 
 
-IS_TRACKING = True
-IS_PERSON = True
-IS_GROUP = True
-IS_DISPLAY = True
+# IS_TRACKING = True
+# IS_PERSON = True
+# IS_GROUP = True
+# IS_DISPLAY = True
 
-ROOM_NUM = '09'
-DATE = '20210304'
-NAME = 'pass1'
+# ROOM_NUM = '09'
+# DATE = '20210304'
+# NAME = 'pass2':
 
-
-if __name__ == '__main__':
-    video_path = os.path.join(common.data_dir, '{0}/{1}/{2}/AlphaPose_{2}.mp4'.format(ROOM_NUM, DATE, NAME))
-    out_dir = os.path.join(common.out_dir, '{0}/{1}/{2}/'.format(ROOM_NUM, DATE, NAME))
+def main(room_num, date, name, is_tracking, is_person, is_group, is_display):
+    video_path = os.path.join(common.data_dir, '{0}/{1}/{2}/AlphaPose_{2}.mp4'.format(room_num, date, name))
+    out_dir = os.path.join(common.out_dir, '{0}/{1}/{2}/'.format(room_num, date, name))
     field_path = os.path.join(common.data_dir, 'field.png')
-    keypoints_path = os.path.join(common.data_dir, '{0}/{1}/{2}/alphapose-results.json'.format(ROOM_NUM, DATE, NAME))
-    tracking_json_path = os.path.join(common.json_dir, '{0}/{1}/{2}/tracking.json'.format(ROOM_NUM, DATE, NAME))
-    person_json_path = os.path.join(common.json_dir, '{0}/{1}/{2}/person.json'.format(ROOM_NUM, DATE, NAME))
-    group_json_path = os.path.join(common.json_dir, '{0}/{1}/{2}/group.json'.format(ROOM_NUM, DATE, NAME))
+    keypoints_path = os.path.join(common.data_dir, '{0}/{1}/{2}/alphapose-results.json'.format(room_num, date, name))
+    tracking_json_path = os.path.join(common.json_dir, '{0}/{1}/{2}/tracking.json'.format(room_num, date, name))
+    person_json_path = os.path.join(common.json_dir, '{0}/{1}/{2}/person.json'.format(room_num, date, name))
+    group_json_path = os.path.join(common.json_dir, '{0}/{1}/{2}/group.json'.format(room_num, date, name))
 
     # homography
     field_raw = cv2.imread(field_path)
-    p_video = common.homo[ROOM_NUM][0]
-    p_field = common.homo[ROOM_NUM][1]
+    p_video = common.homo[room_num][0]
+    p_field = common.homo[room_num][1]
     homo = transform.Homography(p_video, p_field, field_raw.shape)
 
-    if IS_TRACKING:
+    if is_tracking:
         tr.main(keypoints_path, tracking_json_path)
 
-    if IS_PERSON:
+    if is_person:
         pd.main(tracking_json_path, person_json_path, homo)
 
-    if IS_GROUP:
+    if is_group:
         gd.main(person_json_path, group_json_path, homo)
 
-    if IS_DISPLAY:
+    if is_display:
         display(video_path, out_dir, person_json_path, group_json_path, field_raw)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('room_num', type=str)
+    parser.add_argument('date', type=str)
+    parser.add_argument('name', type=str)
+    parser.add_argument('-t', '--tracking', default=False, action='store_true')
+    parser.add_argument('-p', '--person', default=False, action='store_true')
+    parser.add_argument('-g', '--group', default=False, action='store_true')
+    parser.add_argument('-d', '--display', default=False, action='store_true')
+
+    args = parser.parse_args()
+    room_num = args.room_num
+    date = args.date
+    name = args.name
+    is_tracking = args.tracking
+    is_person = args.person
+    is_group = args.group
+    is_display = args.display
+
+    main(room_num, date, name, is_tracking, is_person, is_group, is_display)
