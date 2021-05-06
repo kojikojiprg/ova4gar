17d289877053828f5437b62e6d369a24bf09d449
kojikojiprg <kojikoji.prg@gmail.com>
2021-05-06 09:52:46 +0900

    first commit

---
+++ module/common/default.py
@@ -0,0 +1,18 @@
+# PERSON INDICATORS
+POSITION_DEFAULT = {
+    'size': 20,
+    'ratio': 1.2,
+}
+
+# GROUP INDICATORS
+DENSITY_DEFAULT = {
+    'k': 3,
+}
+ATTENTION_DEFAULT = {
+    'angle': 10,        # 視野角
+    'division': 5,      # 計算するピクセル幅
+}
+PASSING_DEFAULT = {
+    'th': 0.5,          # 確率のしきい値
+    'th_shita': 60,     # cos類似度（向き合っている角度）のしきい値
+}
