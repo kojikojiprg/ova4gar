320f6893b9d26a0cd5226c99a0681b00c00bb862
kojikojiprg <kojikoji.prg@gmail.com>
2021-06-06 10:50:23 +0900

    tuning passing

--- src/common/default.py
+++ src/common/default.py
@@ -15,6 +15,6 @@ ATTENTION_DEFAULT = {
 PASSING_DEFAULT = {
     'th': 0.4,          # 確率のしきい値
     'th_shita': 60,     # cos類似度（向き合っている角度）のしきい値
-    'gauss_mu': 200,    # 距離に対するガウス分布の平均
-    'gauss_sig': 100,    # 距離に対するガウス分布の分散
+    'gauss_mu': 120,    # 距離に対するガウス分布の平均
+    'gauss_sig': 50,    # 距離に対するガウス分布の分散
 }
