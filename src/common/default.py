0191988c28f01cfb2a21ca060b9d7f4d48afb75a
kojikojiprg <kojikoji.prg@gmail.com>
2021-06-24 23:50:58 +0900

    adjust param

--- src/common/default.py
+++ src/common/default.py
@@ -28,6 +28,6 @@ ATTENTION_DEFAULT = {
 PASSING_DEFAULT = {
     'gauss_mu': 100,    # 距離に対するガウス分布の平均
     'gauss_sig': 100,    # 距離に対するガウス分布の分散
-    'C': 86,
-    'gamma': 0.99
+    'C': 54,
+    'gamma': 0.37
 }
