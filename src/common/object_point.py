82ada5141af9a136127040a6e336a05f89b5640a
kojikojiprg <kojikoji.prg@gmail.com>
2021-07-13 15:51:19 +0900

    change ex0304

--- src/common/object_point.py
+++ src/common/object_point.py
@@ -1,6 +1,6 @@
 # for field0304.png
 EX0304 = {
-    1: (710, 812),  # monitor
-    2: (80, 772),   # PC
+    1: (705, 810),  # monitor
+    2: (74, 810),   # PC
     3: (390, 443),  # operating table
 }
