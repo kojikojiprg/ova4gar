4886638b0a2388c5b65a571095f2e79614b41556
kojikojiprg <kojikoji.prg@gmail.com>
2021-12-02 21:11:33 +0900

    modify feature extraction

--- src/common/default.py
+++ src/common/default.py
@@ -1,41 +1,43 @@
 # PERSON INDICATORS
 POSITION_DEFAULT = {
-    'ankle_th': 0.5,    # Ankleキーポイントに対する閾値
-    'size': 30,
-    'ratio': 1.2,
-    'std_th': 1,        # 外れ値に対する分散の閾値
+    "ankle_th": 0.5,  # Ankleキーポイントに対する閾値
+    "size": 30,
+    "ratio": 1.2,
+    "std_th": 1,  # 外れ値に対する分散の閾値
 }
 FACE_DEFAULT = {
-    'size': 20,
-    'ratio': 2.7,       # 耳の位置 / 肩と腰の差分
-    'std_th': 1,        # 外れ値に対する分散の閾値
+    "size": 20,
+    "ratio": 2.7,  # 耳の位置 / 肩と腰の差分
+    "std_th": 1,  # 外れ値に対する分散の閾値
 }
 BODY_DEFAULT = {
-    'size': 20,
-    'ratio': 2.2,       # 肩の位置 / 肩と腰の差分
-    'std_th': 1,        # 外れ値に対する分散の閾値
+    "size": 20,
+    "ratio": 2.2,  # 肩の位置 / 肩と腰の差分
+    "std_th": 1,  # 外れ値に対する分散の閾値
 }
 ARM_DEFAULT = {
-    'size': 5,
-    'std_th': 1,        # 外れ値に対する分散の閾値
+    "size": 5,
+    "std_th": 1,  # 外れ値に対する分散の閾値
 }
 KEYPOINTS_DEFAULT = {
-    'size': 10,
-    'std_th': 1,        # 外れ値に対する分散の閾値
+    "size": 10,
+    "std_th": 1,  # 外れ値に対する分散の閾値
 }
 
 # GROUP INDICATORS
 ATTENTION_DEFAULT = {
     # 'angle': 10,        # 視野角
     # 'division': 5,      # 計算するピクセル幅
-    'angle_th': 10,
-    'count_th': 2
+    "angle_th": 10,
+    "count_th": 2,
 }
 PASSING_DEFAULT = {
-    'gauss_mu': 250,    # 距離に対するガウス分布の平均
-    'gauss_sig': 50,    # 距離に対するガウス分布の分散
-    'C': 54,
-    'gamma': 0.37,
-    'wrist_gauss_mu': 100,    # 手首の距離に対するガウス分布の平均
-    'wrist_gauss_sig': 50,    # 手首の距離に対するガウス分布の分散
+    "gauss_mu": 250,  # 距離に対するガウス分布の平均
+    "gauss_sig": 50,  # 距離に対するガウス分布の分散
+    "gauss_sig_min": 200,  # 距離に対するガウス分布の分散
+    "C": 54,
+    "gamma": 0.37,
+    "wrist_gauss_mu": 100,  # 手首の距離に対するガウス分布の平均
+    "wrist_gauss_sig": 50,  # 手首の距離に対するガウス分布の分散
+    "wrist_gauss_sig_min": 100,  # 手首の距離に対するガウス分布の分散
 }
