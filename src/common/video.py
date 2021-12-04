8a57fdb949ec9d1a344e2e53d143884b3c501f25
kojikojiprg <kojikoji.prg@gmail.com>
2021-12-04 16:54:50 +0900

    add writer

--- src/common/video.py
+++ src/common/video.py
@@ -8,38 +8,42 @@ class Capture:
         if not os.path.isfile(in_path):
             raise ValueError(f"not exist file {in_path}")
 
-        self._video = cv2.VideoCapture(in_path)
+        self._cap = cv2.VideoCapture(in_path)
 
         # video info
-        self.fps = int(self._video.get(cv2.CAP_PROP_FPS))
-        self.frame_num = int(self._video.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.fps = int(self._cap.get(cv2.CAP_PROP_FPS))
+        self.frame_num = int(self._cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     def __del__(self):
-        self._video.release()
+        self._cap.release()
         cv2.destroyAllWindows()
 
     def set_pos_frame(self, begin_sec):
-        self._video.set(cv2.CAP_PROP_POS_FRAMES, begin_sec * self.fps)
+        self._cap.set(cv2.CAP_PROP_POS_FRAMES, begin_sec * self.fps)
 
     def read(self):
-        ret, frame = self._video.read()
+        ret, frame = self._cap.read()
         if ret:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # BGR to RGB
             return frame
         else:
             return None
 
-    def write(self, frames, out_path, size):
+
+class Writer:
+    def __init__(self, out_path, fps, size, fmt="h264"):
         out_dir = os.path.dirname(out_path)
         if not os.path.exists(out_dir):
             os.makedirs(out_dir, exist_ok=True)
 
         # writer object
-        fmt = cv2.VideoWriter_fourcc("h", "2", "6", "4")
-        self._writer = cv2.VideoWriter(out_path, fmt, self.fps, size)
+        fmt = cv2.VideoWriter_fourcc(fmt[0], fmt[1], fmt[2], fmt[3])
+        self._writer = cv2.VideoWriter(out_path, fmt, fps, size)
 
+    def __del__(self):
+        self._writer.release()
+
+    def write(self, frames):
         for frame in frames:
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # RGB to BGR
             self._writer.write(frame)
-
-        self._writer.release()
