6fe19218b647ce72883df928839ff7d0041a47e0
kojikojiprg <kojikoji.prg@gmail.com>
2021-12-15 19:08:01 +0900

    modify for hrnet

--- src/passing.py
+++ src/passing.py
@@ -31,7 +31,7 @@ def main(
 ):
     video_path = os.path.join(
         common.data_dir,
-        "{0}/{1}/{2}/video/AlphaPose_{3}.mp4".format(
+        "{0}/{1}/{2}/video/pose_{3}.mp4".format(
             room_num, date, name, name.replace("passing/", "")
         ),
     )
@@ -41,7 +41,7 @@ def main(
     field_path = os.path.join(common.data_dir, "{}/field.png".format(room_num))
     keypoints_path = os.path.join(
         common.data_dir,
-        "{0}/{1}/{2}/json/alphapose-results.json".format(room_num, date, name),
+        "{0}/{1}/{2}/json/pose-results.json".format(room_num, date, name),
     )
     tracking_json_path = os.path.join(
         common.data_dir, "{0}/{1}/{2}/json/tracking.json".format(room_num, date, name)
