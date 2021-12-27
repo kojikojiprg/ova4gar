6264d9e6c915f5d5a56e8d04ce569e1b155a5e7f
kojikojiprg <kojikoji.prg@gmail.com>
2021-12-27 22:44:37 +0900

    modify

--- src/individual_activity/individual_activity.py
+++ src/individual_activity/individual_activity.py
@@ -1,8 +1,11 @@
-import numpy as np
 from common.json import IA_FORMAT, START_IDX
 from common.keypoint import Keypoints, body
 
-from individual_activity.indicator import INDICATOR_DICT, calc_new_keypoints, fill_nan_keypoints
+from individual_activity.indicator import (
+    INDICATOR_DICT,
+    calc_new_keypoints,
+    fill_nan_keypoints,
+)
 
 
 class IndividualActivity:
@@ -28,7 +31,7 @@ class IndividualActivity:
             frame_num, pre_frame_num, keypoints, self.keypoints_que
         )
         self.keypoints, self.keypoints_que = fill_nan_keypoints(
-            frame_num, pre_frame_num,self.keypoints, self.keypoints_que
+            frame_num, pre_frame_num, self.keypoints, self.keypoints_que
         )
         keypoints = Keypoints(self.keypoints[frame_num])
 
@@ -60,50 +63,6 @@ class IndividualActivity:
             return self.keypoints[frame_num][body[key]]
         else:
             return None
-
-    def get_keypoints_dict(self, window=3, norm_coor=None) -> dict:
-        # fill nan
-        min_frame_num = min(self.keypoints.keys())
-        max_frame_num = max(self.keypoints.keys())
-
-        copy_kps_lst = []
-        pre = np.array(self.keypoints[min_frame_num])
-        for frame_num in range(min_frame_num + 1, max_frame_num + 1):
-            if frame_num in self.keypoints:
-                kps = np.array(self.keypoints[frame_num])
-                if True in np.isnan(kps):
-                    # 一部のnanを前フレームからコピー
-                    kps = np.where(np.isnan(kps), pre, kps).copy()
-                copy_kps_lst.append(kps)
-                pre = kps.copy()
-            else:
-                # 前のフレームからコピー
-                copy_kps_lst.append(pre)
-
-        # 残ったnanは移動平均で穴埋め
-        ma_kps_lst = []
-        for i in range(0, len(copy_kps_lst) - window):
-            means = np.nanmean(copy_kps_lst[i : i + window], axis=0)
-            for kps in copy_kps_lst[i : i + window]:
-                if True in np.isnan(kps):
-                    kps = np.where(np.isnan(kps), means, kps).copy()
-
-                if len(ma_kps_lst) <= i + window:
-                    ma_kps_lst.append(kps)
-
-        ma_kps_dict = {}
-        for i, kps in enumerate(ma_kps_lst):
-            ma_kps_dict[min_frame_num + i] = np.array(kps)
-
-        if norm_coor is not None:
-            # standardize
-            norm_kps_dict = {}
-            for frame_num, kps in ma_kps_dict.items():
-                norm_kps_dict[frame_num] = np.array(kps) / norm_coor
-
-            return norm_kps_dict
-        else:
-            return ma_kps_dict
 
     def to_json(self, frame_num):
         data = {}
