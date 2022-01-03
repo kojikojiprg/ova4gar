9df1b227fa2acf7581d4f9afd0225c66a9eadae8
kojikojiprg <kojikoji.prg@gmail.com>
2022-01-03 21:53:25 +0900

    modify plot point

--- src/display/tracking.py
+++ src/display/tracking.py
@@ -7,7 +7,7 @@ def disp_tracking(individual_activity_datas, frame):
         individual_activity_id = data[IA_FORMAT[0]]
         point = data[IA_FORMAT[2]]
         if point is not None:
-            cv2.circle(frame, tuple(point), 7, (0, 0, 255), thickness=-1)
+            cv2.circle(frame, tuple(point), 10, (100, 100, 255), thickness=-1)
             cv2.putText(
                 frame,
                 str(individual_activity_id),
