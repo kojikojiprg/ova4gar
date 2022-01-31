5fbbf7b8fb46d4bb956deb08a1472ec39bdaf486
kojikojiprg <kojikoji.prg@gmail.com>
2022-01-31 20:15:26 +0900

    modify

--- src/group_activity/r/attention_mwu_test.r
+++ src/group_activity/r/attention_mwu_test.r
@@ -1,9 +1,9 @@
 setwd('C:/Users/k2111/program/research/data/csv/attention')
+
 files <- list.files()
 for (path in files) {
   dat <- read.csv(path, header=TRUE)
-  result <- wilcox.test(dat$pos, dat$nega, paired=F, alternative='g')
-  pval <- result$p.value
+  result <- wilcox.test(dat$pos, dat$nega, paired=F, alternative='g', exact=T)
   print(path)
-  print(pval)
+  print(result)
 }
