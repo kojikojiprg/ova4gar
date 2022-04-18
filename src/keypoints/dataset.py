ac31cca76148322f601b130cb03a1e22806759d7
kojikojiprg <kojikoji.prg@gmail.com>
2022-04-19 05:21:34 +0900

    first commit

---
+++ src/keypoints/dataset.py
@@ -0,0 +1,31 @@
+import sys
+
+import torch
+
+sys.path.append("src")
+from utils.video import Capture
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, video_path: str):
+        self.cap = Capture(video_path)
+        assert (
+            self.cap.isOpened()
+        ), f"{video_path} does not exist or is wrong file type."
+
+    def __len__(self):
+        return len(self.cap)
+
+    def __getitem__(self, idx):
+        self.cap.set_pos_frame_count(idx)
+        return self.cap.read()
+
+
+def make_dataloader(video_path: str):
+    dataset = Dataset(video_path)
+
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=False
+    )
+
+    return dataset, data_loader
