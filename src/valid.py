631454b6f41ffc1148fec5fed14022073d9ef58d
kojikojiprg <kojikoji.prg@gmail.com>
2022-04-19 05:44:39 +0900

    first commit

---
+++ src/valid.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+
+from keypoints.hrnet import HRNetExtractor
+from utils.logger import setup_logger
+
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("video_path", type=str, help="Path of a raw video.")
+    parser.add_argument("data_dir_root", type=str, help="Path of data directory where results are saved.")
+    parser.add_argument(
+        "--hrnet_cfg",
+        type=str,
+        default="./submodules/hrnet/coco/higher_hrnet/w32_512_adam_lr1e-3.yaml",
+        help="The output data directory path.",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parser()
+    video_path = args.video_path
+    data_dir = args.data_dir
+    hrnet_cfg_path = args.hrnet_cfg
+
+    # create data dir
+    os.makedirs(data_dir, exist_ok=True)
+
+    # create logger
+    log_dir = os.path.join(data_dir, "log")
+    os.makedirs(log_dir, exist_ok=True)
+    logger = setup_logger(log_dir)
+
+    # extract keypoints
+    extractor = HRNetExtractor(hrnet_cfg_path, logger)
+    extractor.predict(video_path, data_dir)
+
+
+if __name__ == "__main__":
+    main()
