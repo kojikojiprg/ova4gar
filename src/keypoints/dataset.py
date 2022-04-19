b74d911f9902a6d52f2453a52f502ba18412b704
Koji Yokoyama <kojikoji.prg@gmail.com>
2022-04-19 14:03:45 +0900

    delete cuda setting

--- src/keypoints/dataset.py
+++ src/keypoints/dataset.py
@@ -10,14 +10,13 @@ class TestDataset(torch.utils.data.Dataset):
     def __init__(self, video_path: str):
         self.cap = Capture(video_path)
         assert self.cap.is_opened, f"{video_path} does not exist or is wrong file type."
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
     def __len__(self):
         return len(self.cap)
 
     def __getitem__(self, idx):
         self.cap.set_pos_frame_count(idx)
-        return torch.tensor(self.cap.read()).to(self.device)
+        return self.cap.read()
 
 
 def make_test_dataloader(video_path: str):
