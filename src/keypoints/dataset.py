125c48dbda3058730a08d857c0e08aa81c0119e2
Koji Yokoyama <kojikoji.prg@gmail.com>
2022-04-19 13:49:41 +0900

    modify getitem and make_test_loader

--- src/keypoints/dataset.py
+++ src/keypoints/dataset.py
@@ -9,16 +9,15 @@ from utils.video import Capture
 class TestDataset(torch.utils.data.Dataset):
     def __init__(self, video_path: str):
         self.cap = Capture(video_path)
-        assert (
-            self.cap.is_opened
-        ), f"{video_path} does not exist or is wrong file type."
+        assert self.cap.is_opened, f"{video_path} does not exist or is wrong file type."
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
     def __len__(self):
         return len(self.cap)
 
     def __getitem__(self, idx):
         self.cap.set_pos_frame_count(idx)
-        return self.cap.read()
+        return torch.tensor(self.cap.read()).to(self.device)
 
 
 def make_test_dataloader(video_path: str):
@@ -28,4 +27,4 @@ def make_test_dataloader(video_path: str):
         dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=False
     )
 
-    return dataset, data_loader
+    return data_loader, dataset
