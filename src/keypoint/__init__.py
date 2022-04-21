7db05d5d27b92c2f34dada934f5d7f9244dcd4dd
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-04-21 11:20:32 +0900

    first commit

---
+++ src/keypoint/__init__.py
@@ -0,0 +1,15 @@
+import sys
+import importlib
+
+# hrnet and unitrack have "uitls" module.
+# Avoid import error.
+sys.path.append("./submodules/hrnet/lib/")
+from . import hrnet
+
+sys.path.remove("./submodules/hrnet/lib/")
+sys.path.append("./submodules/unitrack/")
+import utils
+importlib.reload(utils)
+from . import unitrack
+
+from . import extracter
