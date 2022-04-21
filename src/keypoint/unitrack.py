1582ade13b3f80c382ffd90a86092c4ac5e81dd9
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-04-21 12:49:03 +0900

    modify image preprocessing

--- src/keypoint/unitrack.py
+++ src/keypoint/unitrack.py
@@ -1,17 +1,19 @@
 import pprint
+from distutils.log import Log
+from logging import Logger
 from types import SimpleNamespace
+from typing import Dict, List
 
-import cv2
-import numpy as np
 import yaml
+from numpy.typing import NDArray
 from torchvision.transforms import transforms as T
-
-from data.video import letterbox
-from tracker.mot.pose import PoseAssociationTracker
+from tracker.mot.pose import PoseAssociationTracker  # from unitrack
 
 
 class UniTrackTracker:
-    def __init__(self, cfg_path, logger, img_size=[640, 480], conf_thres=0.65):
+    def __init__(
+        self, cfg_path: str, logger: Logger, model_path: str = "models/unitrack/crw.pth"
+    ):
         # set config
         opts = SimpleNamespace(**{})
         with open(cfg_path) as f:
@@ -20,9 +22,8 @@ class UniTrackTracker:
             setattr(opts, k, v)
         for k, v in common_args["posetrack"].items():
             setattr(opts, k, v)
-        opts.img_size = img_size
-        opts.conf_thres = conf_thres
-        opts.resume = "models/unitrack/crw.pth"
+        # opts.img_size = img_size
+        opts.resume = model_path
 
         self.logger = logger
         self.logger.info(f"=> unitrack config: {pprint.pformat(opts)}")
@@ -34,35 +35,24 @@ class UniTrackTracker:
             [T.ToTensor(), T.Normalize(opts.im_mean, opts.im_std)]
         )
 
-        self.w = img_size[0]
-        self.h = img_size[1]
-
     def __del__(self):
         del self.tracker, self.transforms
 
-    def update(self, img, kps):
-        tmp_img = cv2.resize(img, (self.w, self.h))
+    def update(self, img: NDArray, img0: NDArray, kps: List[list]):
+        img = img / 255.0
+        img = self.transforms(img)
 
-        # Padded resize
-        tmp_img, _, _, _ = letterbox(tmp_img, height=self.height, width=self.width)
+        obs = [self._cvt_kp2ob(kp) for kp in kps]
 
-        # Normalize RGB
-        tmp_img = img[:, :, ::-1]
-        tmp_img = np.ascontiguousarray(tmp_img, dtype=np.float32)
-
-        tmp_img = tmp_img / 255.0
-        tmp_img = self.transforms(tmp_img)
-
-        obs = [self._cvt_kp(kp) for kp in kps]
-
-        tracks = self.tracker.update(tmp_img, img, obs)
+        tracks = self.tracker.update(img, img0, obs)
         for t in tracks:
-            t.pose = self._cvt_ob2kp(t.pose)
+            if not isinstance(t.pose[0], list):
+                t.pose = self._cvt_ob2kp(t.pose)
 
         return tracks
 
     @staticmethod
-    def _cvt_kp2ob(kp):
+    def _cvt_kp2ob(kp: List[list]):
         # https://github.com/leonid-pishchulin/poseval
         return [
             {"id": [i], "x": [p[0]], "y": [p[1]], "score": [p[2]]}
@@ -70,5 +60,5 @@ class UniTrackTracker:
         ]
 
     @staticmethod
-    def _cvt_ob2kp(ob):
+    def _cvt_ob2kp(ob: List[Dict[str, list]]):
         return [[p["x"][0], p["y"][0], p["score"][0]] for p in ob]
