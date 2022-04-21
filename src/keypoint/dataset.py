5a85be898050b31d71f2d2d5bed93e398b096cdf
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-04-21 12:22:28 +0900

    modify for unitrack

--- src/keypoint/dataset.py
+++ src/keypoint/dataset.py
@@ -1,17 +1,42 @@
+import cv2
+import numpy as np
 import torch
-from utility.video import Capture
+from data.video import letterbox  # from unitrack
+from utility.video import Capture  # from this project
 
 
 class TestDataset(torch.utils.data.Dataset):
-    def __init__(self, video_cap: Capture):
+    def __init__(self, video_cap: Capture, img_size=[640, 480]):
         self.cap = video_cap
         self.cap.set_pos_frame_count(0)
+        vw, vh = self.cap.size
+
+        self.width = img_size[0]
+        self.height = img_size[1]
+        self.w, self.h = self._get_size(vw, vh, self.width, self.height)
 
     def __len__(self):
         return self.cap.frame_count
 
     def __getitem__(self, idx):
-        return self.cap.read()
+        ret, img0 = self.cap.read()
+
+        img = cv2.resize(img0, (self.w, self.h))
+
+        # Padded resize
+        img, _, _, _ = letterbox(img, height=self.height, width=self.width)
+
+        # Normalize RGB
+        img = img[:, :, ::-1]
+        img = np.ascontiguousarray(img, dtype=np.float32)
+
+        return img, img0
+
+    @staticmethod
+    def _get_size(vw, vh, dw, dh):
+        wa, ha = float(dw) / vw, float(dh) / vh
+        a = min(wa, ha)
+        return int(vw * a), int(vh * a)
 
 
 def make_test_dataloader(video_cap: Capture):
