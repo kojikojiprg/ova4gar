894a92ea637454303beb0255dd2c44757a03a101
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-04-21 10:49:44 +0900

    first commit

---
+++ src/keypoint/extracter.py
@@ -0,0 +1,89 @@
+import os
+import sys
+from logging import Logger
+
+import numpy as np
+from tqdm import tqdm
+
+sys.path.append("./src/")
+from utils.json_handler import dump
+from utils.video import Capture, Writer
+from utils.visualization import draw_skeleton
+
+from .dataset import make_test_dataloader
+from .hrnet import HRNetDetecter
+from .unitrack import UniTrackTracker
+
+
+class Extractor:
+    def __init__(self, hrnet_cfg_path: str, hrnet_opts: list, unitrack_opts, logger: Logger):
+        self.detector = HRNetDetecter(hrnet_cfg_path, hrnet_opts, logger)
+        self.tracker = UniTrackTracker(unitrack_opts)
+        self.logger = logger
+
+    def __del__(self):
+        del self.detector, self.logger
+
+    def extract(self, video_path: str, data_dir: str):
+        # create video capture
+        video_capture = Capture(video_path)
+        assert (
+            video_capture.is_opened
+        ), f"{video_path} does not exist or is wrong file type."
+
+        # create video writer
+        out_path = os.path.join(data_dir, "video", "hrnet.mp4")
+        video_writer = Writer(out_path, video_capture.fps, video_capture.size)
+        self.logger.info(f"=> writing video into {out_path} while processing.")
+
+        # prepair json data list
+        json_path = os.path.join(data_dir, "json", "keypoints.json")
+        json_data = []
+
+        data_loader, test_dataset = make_test_dataloader(video_capture)
+        pbar = tqdm(total=len(test_dataset))
+        for frame_num, (rets, images) in enumerate(data_loader):
+            if not rets[0]:
+                self.logger.info(
+                    f"=> couldn't read frame number {frame_num} on video {video_path}."
+                )
+                break
+
+            assert 1 == images.size(0), "Test batch size should be 1"
+            image = images[0].cpu().numpy()
+
+            # do keypoints detection and tracking
+            keypoints = self.detector.predict(image)
+            tracks = self.tracker.update(image, keypoints)
+
+            self._write_video(video_writer, image, tracks)  # write video
+
+            # append result
+            for t in tracks:
+                data = {
+                    "frame": frame_num + 1,
+                    "id": t.track_id,
+                    "keypoints": np.array(t.pose),
+                }
+                json_data.append(data)
+
+            pbar.update()
+
+        pbar.close()
+
+        self.logger.info(f" => writing json file into {json_path}.")
+        self._write_json(json_data, json_path)
+
+        # release memory
+        del (video_capture, video_writer, data_loader, test_dataset, json_data, data)
+
+    def _write_video(self, writer: Writer, image, tracks):
+        # add keypoints to image
+        for t in tracks:
+            draw_skeleton(image, t.t_id, t.pose)
+
+        writer.write(image)
+
+    def _write_json(self, json_data, json_path):
+        os.makedirs(os.path.dirname(json_path), exist_ok=True)
+        dump(json_data, json_path)
---
+++ src/keypoint/unitrack.py
@@ -0,0 +1,69 @@
+import sys
+from types import SimpleNamespace
+
+import cv2
+import numpy as np
+import yaml
+from torchvision.transforms import transforms as T
+
+sys.path.append("./submodules/unitrack/")
+from data.video import letterbox
+from tracker.mot.pose import PoseAssociationTracker
+
+
+class UniTrackTracker:
+    def __init__(self, cfg_path, img_size=[640, 480], conf_thres=0.65):
+        # set config
+        opts = SimpleNamespace(**{})
+        with open(cfg_path) as f:
+            common_args = yaml.load(f)
+        for k, v in common_args["common"].items():
+            setattr(opts, k, v)
+        opts.img_size = img_size
+        opts.conf_thres = conf_thres
+        opts.resume = "./models/unitrack/crw.pth"
+
+        self.tracker = PoseAssociationTracker(opts)
+
+        self.transforms = T.Compose(
+            [T.ToTensor(), T.Normalize(opts.im_mean, opts.im_std)]
+        )
+
+        self.w = img_size[0]
+        self.h = img_size[1]
+
+    def __del__(self):
+        del self.tracker, self.transforms
+
+    def update(self, img, kps):
+        tmp_img = cv2.resize(img, (self.w, self.h))
+
+        # Padded resize
+        tmp_img, _, _, _ = letterbox(tmp_img, height=self.height, width=self.width)
+
+        # Normalize RGB
+        tmp_img = img[:, :, ::-1]
+        tmp_img = np.ascontiguousarray(tmp_img, dtype=np.float32)
+
+        tmp_img = tmp_img / 255.0
+        tmp_img = self.transforms(tmp_img)
+
+        obs = [self._cvt_kp(kp) for kp in kps]
+
+        tracks = self.tracker.update(tmp_img, img, obs)
+        for t in tracks:
+            t.pose = self._cvt_ob2kp(t.pose)
+
+        return tracks
+
+    @staticmethod
+    def _cvt_kp2ob(kp):
+        # https://github.com/leonid-pishchulin/poseval
+        return [
+            {"id": [i], "x": [p[0]], "y": [p[1]], "score": [p[2]]}
+            for i, p in enumerate(kp)
+        ]
+
+    @staticmethod
+    def _cvt_ob2kp(ob):
+        return [[p["x"][0], p["y"][0], p["score"][0]] for p in ob]
