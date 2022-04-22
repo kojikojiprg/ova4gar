a20384d2d36d8a974ac871678768414267d102cb
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-04-22 14:26:59 +0900

    delete

--- src_backup/group_activity/r/attention_mwu_test.r
+++
@@ -1,9 +0,0 @@
-setwd('C:/Users/k2111/program/research/data/csv/attention')
-
-files <- list.files()
-for (path in files) {
-  dat <- read.csv(path, header=TRUE)
-  result <- wilcox.test(dat$pos, dat$nega, paired=F, alternative='g', exact=T)
-  print(path)
-  print(result)
-}
