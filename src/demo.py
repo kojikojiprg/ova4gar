e941a374edaf5baf8810905899cbb7f998e06ceb
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-04-28 20:45:23 +0900

    modify for multiple files

--- src/demo.py
+++ src/demo.py
@@ -1,8 +1,8 @@
 import argparse
 import os
+from glob import glob
 
 import cv2
-import numpy as np
 import yaml
 
 from group.group_analyzer import GroupAnalyzer
@@ -14,23 +14,23 @@ from utility.transform import Homography
 
 def parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("video_path", type=str, help="Path of a raw video.")
+    # requires
     parser.add_argument(
-        "data_dir", type=str, help="Path of data directory where results are saved."
+        "-rn",
+        "--room_num",
+        required=True,
+        type=str,
+        help="The room number of operating room",
     )
+    parser.add_argument("-d", "--date", required=True, type=str, help="Date")
+
+    # options
     parser.add_argument(
         "-c",
         "--cfg_path",
         type=str,
         default="config/config.yaml",
         help="Config file path.",
-    )
-    parser.add_argument(
-        "-rn",
-        "--room_num",
-        type=str,
-        default="02",
-        help="The room number of operating room",
     )
     parser.add_argument(
         "-wk",
@@ -58,17 +58,23 @@ def parser():
 
 def main():
     args = parser()
-    video_path = args.video_path
-    data_dir = args.data_dir
-    cfg_path = args.cfg_path
     room_num = args.room_num
+    date = args.date
+    cfg_path = args.cfg_path
     without_keypoint = args.without_keypoint
     without_individual = args.without_individual
     without_group = args.without_group
 
-    # create data dir
-    os.makedirs(data_dir, exist_ok=True)
+    video_dir = os.path.join("video", room_num, date)
+    video_paths = glob(os.path.join(video_dir, "*.mp4"))
+    data_dirs = []
+    for video_path in video_paths:
+        name = os.path.basename(video_path).replace(".mp4", "")
+        data_dir = os.path.join("data", room_num, date, name)
+        data_dirs.append(data_dir)
+        os.makedirs(data_dir, exist_ok=True)
 
+    # open config file
     with open(cfg_path) as f:
         cfg = yaml.safe_load(f)
 
@@ -85,20 +91,21 @@ def main():
     os.makedirs(log_dir, exist_ok=True)
     logger = setup_logger(log_dir)
 
-    # extract keypoints
-    if not without_keypoint:
-        extractor = Extractor(cfg, logger)
-        extractor.predict(video_path, data_dir)
+    for video_path, data_dir in zip(video_paths, data_dirs):
+        # extract keypoints
+        if not without_keypoint:
+            extractor = Extractor(cfg, logger)
+            extractor.predict(video_path, data_dir)
 
-    # individual actitivy
-    if not without_individual:
-        individual_anlyzer: IndividualAnalyzer = IndividualAnalyzer(cfg, logger)
-        individual_anlyzer.analyze(data_dir, homo, field)
+        # individual actitivy
+        if not without_individual:
+            individual_anlyzer: IndividualAnalyzer = IndividualAnalyzer(cfg, logger)
+            individual_anlyzer.analyze(data_dir, homo, field)
 
-    # group actitivy
-    if not without_group:
-        group_anlyzer: GroupAnalyzer = GroupAnalyzer(cfg, logger)
-        group_anlyzer.analyze(data_dir, field)
+        # group actitivy
+        if not without_group:
+            group_anlyzer: GroupAnalyzer = GroupAnalyzer(cfg, logger)
+            group_anlyzer.analyze(data_dir, field)
 
 
 if __name__ == "__main__":
