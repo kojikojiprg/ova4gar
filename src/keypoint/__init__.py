b5617eaa0bc8867bdd6eb40d87a5e0fed605f285
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-04-28 21:53:48 +0900

    add hrnet

--- src/keypoint/__init__.py
+++ src/keypoint/__init__.py
@@ -1,15 +1,21 @@
 import importlib
 import sys
 
-# hrnet and unitrack have "uitls" module.
+# all submodules have "uitls" module.
 # Avoid import error.
 sys.path.append("submodules/higher-hrnet/lib/")
 from .api import higher_hrnet
 
 sys.path.remove("submodules/higher-hrnet/lib/")
+sys.path.append("submodules/hrnet/lib/")
+import utils
+importlib.reload(utils)
+from .api import hrnet
+
+sys.path.remove("submodules/hrnet/lib/")
 sys.path.append("submodules/unitrack/")
 import utils
-
 importlib.reload(utils)
-from . import dataset, extracter
 from .api import unitrack
+
+from . import dataset, extracter
