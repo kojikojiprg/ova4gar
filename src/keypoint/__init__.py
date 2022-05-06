55b46945181b8b9f2c338465e3acd2ace2971c9c
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-05-06 18:57:08 +0900

    change root

--- src/keypoint/__init__.py
+++ src/keypoint/__init__.py
@@ -1,6 +1,9 @@
 import importlib
 import logging
+import os
 import sys
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # disable matplotlib logging
 logging.getLogger("matplotlib").setLevel(logging.INFO)
@@ -8,25 +11,25 @@ import matplotlib
 
 del matplotlib
 
-sys.path.append("submodules")
+sys.path.append(os.path.join(root, "submodules"))
 
 # Avoid import error.
-sys.path.append("submodules/higher_hrnet/lib/")
+sys.path.append(os.path.join(root, "submodules/higher_hrnet/lib/"))
 from .api import higher_hrnet
 
-sys.path.remove("submodules/higher_hrnet/lib/")
-sys.path.append("submodules/hrnet/lib/")
+sys.path.remove(os.path.join(root, "submodules/higher_hrnet/lib/"))
+sys.path.append(os.path.join(root, "submodules/hrnet/lib/"))
 import models
 
 importlib.reload(models)
 from .api import hrnet
 
-sys.path.remove("submodules/hrnet/lib/")
-sys.path.append("submodules/unitrack/")
+sys.path.remove(os.path.join(root, "submodules/hrnet/lib/"))
+sys.path.append(os.path.join(root, "submodules/unitrack/"))
 import utils
 
 importlib.reload(utils)
 from .api import unitrack
 
-sys.path.remove("submodules/unitrack/")
+sys.path.remove(os.path.join(root, "submodules/unitrack/"))
 from . import dataset, extracter
