e0cbbb741869a27249ffc86cbc6b438cf1928c6a
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2022-06-18 15:28:29 +0900

    add delete time bar height

--- src/visualize/keypoint.py
+++ src/visualize/keypoint.py
@@ -37,7 +37,7 @@ def write_video(video_path, data_dir, start_frame_num, end_frame_num):
 
 
 def write_frame(
-    frame: NDArray, kps_data: List[Dict[str, Any]], frame_num: int, delete_height: int
+    frame: NDArray, kps_data: List[Dict[str, Any]], frame_num: int, delete_height: int = 20
 ):
     # add keypoints to image
     frame = _put_frame_num(frame, frame_num)
